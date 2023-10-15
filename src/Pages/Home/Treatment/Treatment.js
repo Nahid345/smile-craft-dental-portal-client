c730c981c5a32d29004acf89c42960f7eaab283b
Nahid345 <nahidparvej07@gmail.com>
2023-10-15 13:53:58 +0600

    appoinment section design

--- src/Pages/Home/Treatment/Treatment.js
+++ src/Pages/Home/Treatment/Treatment.js
@@ -4,10 +4,10 @@ import treatment from '../../../assets/images/treatment.png'
 
 const Treatment = () => {
     return (
-        <div className=" flex  bg-base-100 shadow-xl mx-auto mt-5 ">
-            <figure><img className='w-80 ml-28 my-20 rounded-md' src={treatment} alt="Movie" /></figure>
-            <div className="ml-20 my-40">
-                <h2 className="font-bold text-3xl mb-3">Exceptional Dental <br /> Care, on Your Terms</h2>
+        <div className=" flex sm:flex-row flex-col  bg-base-100 shadow-xl mx-auto mt-5  ">
+            <figure><img className='w-80 ml-28 my-20  rounded-md' src={treatment} alt="Movie" /></figure>
+            <div className="ml-20 my-40  ">
+                <h2 className="font-bold text-3xl mb-3 ">Exceptional Dental <br /> Care, on Your Terms</h2>
                 <p>It is a long established fact that a reader will be distracted by the readable <br /> content of a page when looking at its layout. The point of using  Lorem Ipsumis <br /> that it has a more-or-less normal distribution of letters,as opposed to using   <br /> 'Content here, content here', making it  look like readable English. Many desktop  <br />publishing packages and web page</p>
                
                 <div>
